04aaf0a9bfbe996c5c14afd42e11c129422f6d97
Star Bots Tamil <126967523+Star-Bots-Tamil@users.noreply.github.com>
2024-04-20 14:08:24 +0530

    Update file_id.py

--- plugins/file_id.py
+++ plugins/file_id.py
@@ -23,30 +23,30 @@ async def handle_new_user_text(bot, message: Message):
 @Client.on_message(filters.video)
 async def handle_video(bot, message: Message):
     # Handle video message
-    await message.reply_text(f"**Video File ID :- `{message.video.file_id}`\n\n**", quote=true)
+    await message.reply_text(f"**Video's File ID :- `{message.video.file_id}`\n\n©️ [Star Bots Tamil](https://t.me/Star_Bots_Tamil)**", quote=true)
 
 @Client.on_message(filters.sticker)
 async def handle_sticker(bot, message: Message):
     # Handle sticker message
-    await message.reply_text(f"Sticker file ID: {message.sticker.file_id}")
+    await message.reply_text(f"**Sticker's File ID :- `{message.sticker.file_id}`\n\n©️ [Star Bots Tamil](https://t.me/Star_Bots_Tamil)**", quote=true)
 
 @Client.on_message(filters.photo)
 async def handle_photo(bot, message: Message):
     # Handle photo message
-    await message.reply_text(f"Photo file ID: {message.photo.file_id}")
+    await message.reply_text(f"**Photo's File ID :- `{message.photo.file_id}`\n\n©️ [Star Bots Tamil](https://t.me/Star_Bots_Tamil)**", quote=true)
 
 @Client.on_message(filters.document)
 async def handle_document(bot, message: Message):
     # Handle document message
-    await message.reply_text(f"Document file ID: {message.document.file_id}")
+    await message.reply_text(f"**Document's File ID :- `{message.document.file_id}`\n\n©️ [Star Bots Tamil](https://t.me/Star_Bots_Tamil)**", quote=true)
 
 # Define handlers for voice and audio messages
 @Client.on_message(filters.voice)
 async def handle_voice(bot, message: Message):
     # Handle voice message
-    await message.reply_text(f"Voice file ID: {message.voice.file_id}")
+    await message.reply_text(f"**Voice's File ID :- `{message.voice.file_id}`\n\n©️ [Star Bots Tamil](https://t.me/Star_Bots_Tamil)**", quote=true)
 
 @Client.on_message(filters.audio)
 async def handle_audio(bot, message: Message):
     # Handle audio message
-    await message.reply_text(f"Audio file ID: {message.audio.file_id}")
+    await message.reply_text(f"**Audio's File ID :- `{message.audio.file_id}`\n\n©️ [Star Bots Tamil](https://t.me/Star_Bots_Tamil)**", quote=true)
